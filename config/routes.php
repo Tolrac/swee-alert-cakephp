3fe14a80174d516ff9eaa38e6cc07842de89ff19
Tolrac <dg-design@outlook.com>
2025-02-19 21:37:33 +0100

    First commit

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,15 @@
 <?php
+
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
-Router::plugin(
-    'SweetAlertHelper',
-    ['path' => '/sweet-alert-helper'],
-    function (RouteBuilder $routes) {
-        $routes->fallbacks(DashedRoute::class);
-    }
-);
+Router::scope('/', function (RouteBuilder $routes) {
+    $routes->plugin(
+        'SweetAlertHelper',
+        ['path' => '/sweet-alert-helper'],
+        function (RouteBuilder $routes) {
+            $routes->fallbacks(DashedRoute::class);
+        }
+    );
+});
